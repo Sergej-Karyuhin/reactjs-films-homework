b5c884b378d5fe6b46a68a2a651a5459789a51e2
Марина Шмовганец <karyuhin.sergej@gmail.com>
2020-10-25 16:03:02 +0300

    feat: add footer

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 
 import Header from '../../containers/Header';
 import MoviesSection from '../../containers/MoviesSection';
+import Footer from '../../components/Footer';
 
 const MainPage = () => (
   <div>
     <Header />
     <MoviesSection />
+    <Footer />
   </div>
 );
 
