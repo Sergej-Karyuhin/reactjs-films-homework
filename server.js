922efc07eb12c2a11cad67f4819caf0b454561ac
Марина Шмовганец <karyuhin.sergej@gmail.com>
2020-10-25 16:24:44 +0300

    fix: change port, delete signature

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const open = require('open');
 const app = express();
 const compiler = webpack(config);
 
-const PORT = 3100;
+const PORT = 3000;
 
 if (process.env.NODE_ENV === 'development') {
   app.use(
