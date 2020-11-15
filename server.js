80bf194bfac106085a3973227db668065b2534da
Марина Шмовганец <karyuhin.sergej@gmail.com>
2020-11-15 15:32:39 +0300

    fix: problem with server

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const webpackDevMiddleware = require('webpack-dev-middleware');
 const webpackHotMiddleware = require('webpack-hot-middleware');
 const config = require('./config/webpack/webpack.dev.config');
 const open = require('open');
+const path = require('path');
 
 const app = express();
 const compiler = webpack(config);
@@ -17,8 +18,24 @@ if (process.env.NODE_ENV === 'development') {
     }),
   );
   app.use(webpackHotMiddleware(compiler));
-} else {
+  app.use('*', (req, res, next) => {
+    const filename = path.join(compiler.outputPath, '/index.html');
+    compiler.outputFileSystem.readFile(filename, (err, result) => {
+      if (err) {
+        return next(err);
+      }
+      res.set('content-type', 'text/html');
+      res.send(result);
+      return res.end();
+    });
+  });
+}
+
+else {
   app.use(express.static('build'));
+  app.get('*', (req, res) => {
+    res.sendFile(`${__dirname}/build/index.html`);
+  });
 }
 
 app.listen(PORT, () => {
