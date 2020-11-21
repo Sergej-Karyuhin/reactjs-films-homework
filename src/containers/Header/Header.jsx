04d3522ed943f860760b8872696bd1b2669b028b
Марина Шмовганец <karyuhin.sergej@gmail.com>
2020-11-21 11:46:40 +0300

    refactor: Header (class => func)

--- src/containers/Header/Header.jsx
+++ src/containers/Header/Header.jsx
@@ -1,38 +1,34 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types'
 
 import SearchInput from '../../components/$Header/SearchInput';
 import styles from './Header.scss';
 
-class Header extends Component {
-  getSearch = async (event) => {
+const Header = ({ setMoviesCondition, condition, history, location }) => {
+  const getSearch = async (event) => {
     const inputValue = event.target.value;
-    const { setMoviesCondition, history } = this.props;
-
     if (inputValue !== '') {
       await setMoviesCondition('Search');
       history.push(`/?search=${inputValue}`);
     }
     else {
       await setMoviesCondition('Trending');
-      const { condition } = this.props;
+      const condition = condition;
       history.push(`/?filter=${condition}`);
     }
   }
 
-  render() {
-    const { location: { search } } = this.props;
-    const searchParams = new URLSearchParams(search);
-    const inputValue = searchParams.get('search') || '';
+  const { search } = location;
+  const searchParams = new URLSearchParams(search);
+  const inputValue = searchParams.get('search') || '';
 
-    return (
-      <header className={styles.header}>
-        <h1 className={styles.title}>films</h1>
-        <SearchInput getSearch={this.getSearch} inputValue={inputValue} />
-      </header>
-    );
-  }
+  return (
+    <header className={styles.header}>
+      <h1 className={styles.title}>films</h1>
+      <SearchInput getSearch={getSearch} inputValue={inputValue} />
+    </header>
+  );
 }
 
 Header.propTypes = {
